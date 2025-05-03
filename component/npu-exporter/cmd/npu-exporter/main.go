22a3d6e4a02e0354b7f22c02c6fba0ae133ad3bf
chaolihf <lichao01>
2025-05-03 21:59:10 +0800

    docs(npu-exporter): 添加参考文档并增强主要功能
    - 在 README.md 中添加了 DCMI 接口文档的链接
    - 在 main.go 中增加了对设备信息的详细获取和打印，包括：
      - 设备数量和列表
      - 每个设备的逻辑 ID
      - 电压信息
      - 功率信息
      - 温度信息
      - 高带宽内存信息
      - 内存信息

--- component/npu-exporter/cmd/npu-exporter/main.go
+++ component/npu-exporter/cmd/npu-exporter/main.go
@@ -28,10 +28,68 @@ func NpuServer(server *http.Server) {
 		fmt.Printf("new npu collector failed, error is %v", err)
 		return
 	}
-	cardNum, cardIDList, err := dmgr.DcMgr.DcGetCardList()
+	deviceManager := dmgr.DcMgr
+	cardNum, cardIDList, err := deviceManager.DcGetCardList()
 	if err != nil {
 		fmt.Printf("error on init %s\n", err)
 		return
 	}
 	fmt.Printf("cardNum %d, cardIDList %v\n", cardNum, cardIDList)
+	for _, cardID := range cardIDList {
+		deviceNum, err := deviceManager.DcGetDeviceNumInCard(cardID)
+		if err != nil {
+			fmt.Printf("error on get device id %s\n", err)
+			return
+		}
+		fmt.Printf("cardID %d, deviceNum %d\n", cardID, deviceNum)
+		for deviceID := 0; deviceID < int(deviceNum); deviceID++ {
+			logicID, err := deviceManager.DcGetDeviceLogicID(cardID, int32(deviceID))
+			if err != nil {
+				fmt.Printf("error on get device logic id %s\n", err)
+				return
+			}
+			fmt.Printf("cardID %d, deviceID %d,logicId %d\n", cardID, deviceID, logicID)
+			newCardId, newDeviceId, err := deviceManager.DcGetCardIDDeviceID(logicID)
+			if err != nil {
+				fmt.Printf("error on get device real device id %s\n", err)
+				return
+			}
+			fmt.Printf("new cardID %d, deviceID %d,logicId %d\n", newCardId, newDeviceId, logicID)
+
+			voltageInfo, err := deviceManager.DcGetDeviceVoltage(cardID, int32(deviceID))
+			if err != nil {
+				fmt.Printf("error on get device voltageInfo id %s\n", err)
+				return
+			}
+			fmt.Printf("cardID %d, deviceID %d,voltageInfo %v\n", cardID, deviceID, voltageInfo)
+
+			powerInfo, err := deviceManager.DcGetDevicePowerInfo(cardID, int32(deviceID))
+			if err != nil {
+				fmt.Printf("error on get device powerInfo id %s\n", err)
+				return
+			}
+			fmt.Printf("cardID %d, deviceID %d,powerInfo %v\n", cardID, deviceID, powerInfo)
+
+			temperatureInfo, err := deviceManager.DcGetDeviceTemperature(cardID, int32(deviceID))
+			if err != nil {
+				fmt.Printf("error on get device temperatureInfo id %s\n", err)
+				return
+			}
+			fmt.Printf("cardID %d, deviceID %d,temperatureInfo %v\n", cardID, deviceID, temperatureInfo)
+
+			highBandwidthMemoryInfo, err := deviceManager.DcGetHbmInfo(cardID, int32(deviceID))
+			if err != nil {
+				fmt.Printf("error on get device highBandwidthMemoryInfo id %s\n", err)
+				return
+			}
+			fmt.Printf("cardID %d, deviceID %d,highBandwidthMemoryInfo %v\n", cardID, deviceID, highBandwidthMemoryInfo)
+
+			memoryInfo, err := deviceManager.DcGetMemoryInfo(cardID, int32(deviceID))
+			if err != nil {
+				fmt.Printf("error on get device memory id %s\n", err)
+				//return
+			}
+			fmt.Printf("cardID %d, deviceID %d,memory %v\n", cardID, deviceID, memoryInfo)
+		}
+	}
 }
